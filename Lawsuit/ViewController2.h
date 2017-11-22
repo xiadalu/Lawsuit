df803913bf42e5db9ca40b386b3d27759d2628bf
xiadalu <937863051@qq.com>
2017-11-22 16:24:42 +0800

    新建一个viewcontroller2

---
+++ Lawsuit/ViewController2.h
@@ -0,0 +1,13 @@
+//
+//  ViewController2.h
+//  Lawsuit
+//
+//  Created by xiadalu on 2017/11/22.
+//  Copyright © 2017年 xiadalu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController2 : UIViewController
+
+@end

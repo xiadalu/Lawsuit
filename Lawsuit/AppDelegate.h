ac6d0366125c1fc8045e8e358d920bdcc7e180ec
xiadalu <937863051@qq.com>
2017-11-22 15:15:51 +0800

    添加立案诉调项目

---
+++ Lawsuit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lawsuit
+//
+//  Created by xiadalu on 2017/11/22.
+//  Copyright © 2017年 xiadalu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lawsuit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lawsuit
+//
+//  Created by xiadalu on 2017/11/22.
+//  Copyright © 2017年 xiadalu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

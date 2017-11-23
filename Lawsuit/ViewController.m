09e26c21d22dc5de752bca33af63485520250311
xiadalu <937863051@qq.com>
2017-11-23 14:32:26 +0800

    添加了pods

--- Lawsuit/ViewController.m
+++ Lawsuit/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-#import "ViewController2.h"
+
 @interface ViewController ()
 
 @end
@@ -16,25 +16,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor whiteColor];
-    UIButton* btn = [UIButton buttonWithType:UIButtonTypeCustom];
-    btn.frame = CGRectMake(100, 200, 200, 100);
-    [btn setTitle:@"我说" forState:UIControlStateNormal];
-    [btn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
-    [self.view addSubview:btn];
-    
-    UIButton* btn2 = [UIButton buttonWithType:UIButtonTypeCustom];
-    btn2.frame = CGRectMake(100, 100, 200, 100);
-    [btn2 setTitle:@"你是" forState:UIControlStateNormal];
-    [btn2 setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
-    [self.view addSubview:btn2];
+   
 }
 
--(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-    ViewController2* vc2 = [[ViewController2 alloc] init];
-    UINavigationController* nav = [[UINavigationController alloc] initWithRootViewController:vc2];
-    [self presentViewController:nav animated:YES completion:nil];
-}
+
 
 
 
